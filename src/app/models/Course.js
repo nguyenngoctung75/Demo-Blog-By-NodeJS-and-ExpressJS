f44d2baa91c1a260fc1e9385ad3989a92fc4c7ab
nguyenngoctung75 <tungit1huster@gmail.com>
2024-12-10 14:22:23 +0700

    test

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose')
-const slug = require('mongoose-slug-updater')
-const mongooseDelete = require('mongoose-delete')
-const AutoIncrement = require('mongoose-sequence')(mongoose)
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+const mongooseDelete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 const Schema = mongoose.Schema;
 
 const CourseSchema = new Schema(
     {
-        _id: { type: Number, },
-        name: { type: String, required: true, },    
-        description: { type: String },  
+        _id: { type: Number },
+        name: { type: String, required: true },
+        description: { type: String },
         image: { type: String },
-        videoID: { type: String, required: true, },
+        videoID: { type: String, required: true },
         level: { type: String },
         slug: { type: String, slug: 'name', unique: true },
-        }, 
+    },
     {
-        _id:false,
+        _id: false,
         timestamps: true,
-    }
-); 
+    },
+);
 
 //Custom query helpers
 CourseSchema.query.sortable = function (req) {
     if (req.query.hasOwnProperty('_sort')) {
-        const isValidType = ['asc', 'desc'].includes(req.query.type)
+        const isValidType = ['asc', 'desc'].includes(req.query.type);
         return this.sort({
-            [req.query.column]: isValidType ? req.query.type : 'desc'
-        })
+            [req.query.column]: isValidType ? req.query.type : 'desc',
+        });
     }
     return this;
-}
+};
 //add plugins
-mongoose.plugin(slug)
+mongoose.plugin(slug);
 
-CourseSchema.plugin(AutoIncrement)
-CourseSchema.plugin(mongooseDelete, { 
+CourseSchema.plugin(AutoIncrement);
+CourseSchema.plugin(mongooseDelete, {
     overrideMethods: 'all',
-    deletedAt: true, 
+    deletedAt: true,
 });
 
 module.exports = mongoose.model('Course', CourseSchema);
